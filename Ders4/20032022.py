f43ba7fe9a38500ab907865284abeebd783ced31
Ahmet Tarık <79899050+mythredd@users.noreply.github.com>
2022-03-20 09:43:49 +0300

    Add files via upload

---
+++ Ders4/20032022.py
@@ -0,0 +1,58 @@
+import random
+zorluk = input("Bir seviye seçiniz (kolay/orta/zor): ")
+print("********************************************************************************************************")
+print("Bütün seviyelerde sadece 3 hakkınız vardır.")
+print("********************************************************************************************************")
+
+def tahmin_etme(tuttugumsayi):
+    i = 0
+    while True:
+        if i == 3:
+            print("-------------------")
+            print("hakkın bitti")
+            print("-------------------")
+            break
+        elif i == 2:
+            print("-------------------")
+            print("son hakkın")
+            print("-------------------")
+        elif i == 1:
+            print("-------------------")
+            print("2. hakkın")
+            print("-------------------")
+        elif i == 0:
+            print("-------------------")
+            print("ilk hakkın")
+            print("-------------------")
+
+        i += 1
+        a = int(input("Sayıyı tahmin et."))
+        if a < tuttugumsayi:
+            print("-------------------")
+            print("Sayınızı büyütün")
+            print("-------------------")
+        elif a > tuttugumsayi:
+            print("-------------------")
+            print("Sayınız Küçültün")
+            print("-------------------")
+        else:
+            print("Doğru bildin")
+            break
+
+
+if zorluk == "kolay":
+    tuttugumsayi = random.randint(1,10)
+    tahmin_etme(tuttugumsayi)
+elif zorluk == "orta":
+    tuttugumsayi = random.randint(1,50)
+    tahmin_etme(tuttugumsayi)
+elif zorluk == "zor":
+    tuttugumsayi = random.randint(1,100)
+    tahmin_etme(tuttugumsayi)
+else:
+    print("Yanlıs seviye girildi")
+
+
+
+
+
